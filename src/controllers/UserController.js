210291e48a9a36ba50782029a92513782ff3b31f
farbs03 <cgeraldfarber@icloud.com>
2024-06-17 16:09:31 -0400

    Adding users now works - see description
    - It was an issue with axios not encoding the request properly
        - Content type is application/x-www-form-urlencoded
        - Use one of the methods listed here: https://axios-http.com/docs/urlencoded

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,19 +1,40 @@
 import axios from 'axios';
 
 export const AddUserController = (name, zone, userType, restApiLocation) => {
-    return axios({
-        method: 'POST',
-        url: `${restApiLocation}/users-groups`,
-        headers: {
-            'Authorization': `Bearer ${localStorage.getItem('zmt-token')}`
-        },
-        params: {
-            op: 'create_user',
-            name: name,
-            zone: zone,
-            "user-type": userType,
-        }
-    })
+    console.log("Before request")
+    // let req = axios({
+    //     method: 'POST',
+    //     url: `${restApiLocation}/users-groups`,
+    //     headers: {
+    //         'Content-Type': 'application/x-www-form-urlencoded',
+    //         'Authorization': `Bearer ${localStorage.getItem('zmt-token')}`,
+    //         'Test': "Chris"
+    //     },
+    //     params: {
+    //         op: "create_user"
+    //     },
+    //     data: {
+    //         name: name,
+    //         zone: zone,
+    //         "user-type": userType,
+    //     }
+    // })
+    const params = new URLSearchParams()
+    params.append("op", "create_user")
+    params.append("name", name)
+    params.append("zone", zone)
+    params.append("user-type", userType)
+    let req = axios.post(
+        `${restApiLocation}/users-groups`,
+        params,
+        {
+            headers: {
+                // 'content-type': 'application/x-www-form-urlencoded',
+                'Authorization': `Bearer ${localStorage.getItem('zmt-token')}`,
+            }
+        } 
+    )
+    return req
 }
 
 export const ModifyUserPasswordController = (name, zone, newPassword, restApiLocation) => {
